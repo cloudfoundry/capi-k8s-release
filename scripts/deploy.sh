7863d255cbd2be05ad74e6c44848f77cdd240e74
Seth Boyles <sboyles@pivotal.io>
2019-12-03 16:31:04 -0800

    Add deployment for cloud controller worker
    [finishes #169924701](https://www.pivotaltracker.com/story/show/169924701)
    
    Co-authored-by: Seth Boyles <sboyles@pivotal.io>
    Co-authored-by: Chris Tarazi <ctarazi@pivotal.io>
    Co-authored-by: Sannidhi Jalukar <sjalukar@pivotal.io>
    Co-authored-by: Chris Selzo <cselzo@pivotal.io>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ helm upgrade --install capi-database stable/postgresql -f "${SCRIPT_DIR}/postgre
 helm upgrade --install capi-blobstore stable/minio
 
 # Build the capi image and push it to minkube
-docker build -f dockerfiles/cloud_controller_ng/Dockerfile -t $(minikube ip):5000/capi src/
+docker build -f dockerfiles/cloud_controller_ng/Dockerfile -t capi -t $(minikube ip):5000/capi src/
 docker push $(minikube ip):5000/capi
 
 #capi
