28bcc6b42a6e037b401ac9a87b9562ac5ccde05b
Chris Tarazi <ctarazi@pivotal.io>
2019-12-17 17:00:56 -0800

    Notify capi when build succeeds
    Co-authored-by: Chris Tarazi <ctarazi@pivotal.io>
    Co-authored-by: Chris Selzo <cselzo@pivotal.io>

--- src/capi-kpack-watcher/go.mod
+++ src/capi-kpack-watcher/go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/pivotal/kpack v0.0.5
+	k8s.io/api v0.0.0-20190918155943-95b840bb6a1f
 	k8s.io/client-go v0.0.0-20190918160344-1fbdaa4c8d90
 )
--- src/capi-kpack-watcher/main.go
+++ src/capi-kpack-watcher/main.go
@@ -16,10 +16,16 @@ limitations under the License.
 package main
 
 import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	// The import below is needed to authenticate with GCP.
+
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
@@ -29,7 +35,14 @@ import (
 	kpackinformer "github.com/pivotal/kpack/pkg/client/informers/externalversions"
 )
 
+var CAPIHost string = os.Getenv("CAPI_HOST")
+
 func main() {
+	if CAPIHost == "" {
+		panic("CAPI_HOST environment variable must be set")
+	}
+
+	// TODO: kubeconfig is really meant for running locally
 	kubeconfig := os.Getenv("KUBECONFIG")
 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
@@ -46,21 +59,59 @@ func main() {
 	stopper := make(chan struct{})
 	defer close(stopper)
 
+	// TODO: ignore added builds at watcher startup
 	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
 			build := obj.(*kpack.Build)
 
 			log.Printf("New Build added: %s\n\n", build.GetName())
 		},
-		UpdateFunc: func(oldobj, newobj interface{}) {
-			build := newobj.(*kpack.Build)
-			status := build.Status
-
-			log.Printf("Update to Build: %s\n", build.GetName())
-			log.Printf("  status: %+v\n", status.Status)
-			log.Printf("  steps : %+v\n\n", status.StepsCompleted)
-		},
+		UpdateFunc: update,
 	})
 
 	informer.Run(stopper)
+}
+
+func update(oldobj, newobj interface{}) {
+	build := newobj.(*kpack.Build)
+	status := build.Status
+
+	log.Printf("Update to Build: %s\n", build.GetName())
+	log.Printf("  status: %+v\n", status.Status)
+	log.Printf("  steps : %+v\n", status.StepsCompleted)
+	log.Printf("  conditions: %+v\n\n", status.Conditions)
+
+	if status.GetCondition("Succeeded").IsTrue() {
+		labels := build.GetLabels()
+		guid := labels["cloudfoundry.org/build_guid"]
+
+		payload := struct {
+			State string `json:"state"`
+		}{
+			State: "STAGED",
+		}
+		jsonPayload, _ := json.Marshal(&payload)
+
+		uri := fmt.Sprintf("https://api.%s/v3/internal/builds/%s", CAPIHost, guid)
+		req, err := http.NewRequest(http.MethodPatch, uri, bytes.NewReader(jsonPayload))
+		if err != nil {
+			log.Printf("Failed to create request: %v\n", err)
+		}
+
+		req.Header.Add("Content-Type", "application/json")
+
+		tr := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		client := &http.Client{Transport: tr}
+
+		log.Printf("  sent payload: %s\n", jsonPayload)
+
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Printf("Failed to send request: %v\n", err)
+		}
+
+		log.Printf("CAPI repsonse: %v\n", resp)
+	}
 }
