fe9c8089fb79b4dadb6be02d3c13f3bff3b45ec9
Piyali Banerjee <pbanerjee@pivotal.io>
2020-01-15 14:50:44 -0800

    Fix rollout command
    Co-authored-by: Piyali Banerjee <pbanerjee@pivotal.io>
    Co-authored-by: Chris Tarazi <ctarazi@pivotal.io>

--- src/capi-kpack-watcher/scripts/build-and-rollout.sh
+++ src/capi-kpack-watcher/scripts/build-and-rollout.sh
@@ -6,5 +6,5 @@ SCRIPTS_DIR="$(dirname $0)"
 
 "${SCRIPTS_DIR}/deploy.sh"
 
-kubectl rollout restart capi-kpack-watcher
-kubectl rollout status capi-kpack-watcher -w
+kubectl rollout restart deployment/capi-kpack-watcher -n cf-system
+kubectl rollout status deployment/capi-kpack-watcher -w -n cf-system
