99f43a49bea9a2326d89f3f403911ee406f7901c
Tim Downey <tdowney@pivotal.io>
2020-01-14 16:52:19 -0800

    wip - use public images

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,10 +13,10 @@ helm upgrade --install capi-database stable/postgresql -f "${SCRIPT_DIR}/postgre
 helm upgrade --install capi-blobstore stable/minio
 
 # Build the capi image and push it to minkube
-docker build -f "${REPO_BASE_DIR}/dockerfiles/cloud_controller_ng/Dockerfile" -t capi -t $(minikube ip):5000/capi "${REPO_BASE_DIR}/src/"
-docker build -f "${REPO_BASE_DIR}/dockerfiles/capi-kpack-watcher/Dockerfile" -t capi-kpack-watcher -t $(minikube ip):5000/capi-kpack-watcher "${REPO_BASE_DIR}/src/"
-docker push $(minikube ip):5000/capi
-docker push $(minikube ip):5000/capi-kpack-watcher
+docker build -f "${REPO_BASE_DIR}/dockerfiles/cloud_controller_ng/Dockerfile" -t capi -t gcr.io/cf-networking-images/capi-k8s-release/capi "${REPO_BASE_DIR}/src/"
+docker build -f "${REPO_BASE_DIR}/dockerfiles/capi-kpack-watcher/Dockerfile" -t capi-kpack-watcher -t gcr.io/cf-networking-images/capi-k8s-release/capi-kpack-watcher "${REPO_BASE_DIR}/src/"
+docker push gcr.io/cf-networking-images/capi-k8s-release/capi
+docker push gcr.io/cf-networking-images/capi-k8s-release/capi-kpack-watcher
 
 #capi
-helm template "${SCRIPT_DIR}/.." --set-string system_domain=minikube.local -f "${SCRIPT_DIR}/capi-values.yaml" | kubectl apply -f -
+helm template "${SCRIPT_DIR}/.." --set-string system_domain=sys.eirini-dev-3.routing.lol --namespace cf-system -f "${SCRIPT_DIR}/capi-values.yaml" | kubectl apply -f -
