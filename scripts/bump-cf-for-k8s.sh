e9cdb32b4216d768e059a9d9f54ba83ed5785be8
Connor Braa <cbraa@pivotal.io>
2020-02-20 17:59:18 -0800

    new and improved bump script
    Authored-by: Connor Braa <cbraa@pivotal.io>

--- scripts/bump-cf-for-k8s.sh
+++ scripts/bump-cf-for-k8s.sh
@@ -6,15 +6,6 @@ CF_FOR_K8s_DIR="${1-${HOME}/workspace/cf-for-k8s/}"
 SCRIPT_DIR="$(dirname $(realpath $0))"
 BASE_DIR="${SCRIPT_DIR}/.."
 
-pushd "${BASE_DIR}"
-	REF="$(git rev-parse head)"
-popd
-
 pushd "${CF_FOR_K8s_DIR}"
-  tmp="$(mktemp)"
-  ytt -f vendir.yml \
-      -f "${BASE_DIR}/overlays/set-vendir-ref/" \
-      -v ref="${REF}" > ${tmp}
-  cat ${tmp} > vendir.yml
-  vendir sync
+  vendir sync --use-directory config/_ytt_lib/github.com/cloudfoundry/capi-k8s-release="${BASE_DIR}"
 popd
