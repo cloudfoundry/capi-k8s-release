9de63bc8275ef673ee6825eeb358588c7cc5bb1a
Connor Braa <cbraa@pivotal.io>
2020-02-24 10:46:33 -0800

    new and improved bump script
    Authored-by: Connor Braa <cbraa@pivotal.io>

---
+++ scripts/bump-cf-for-k8s.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -ex
+
+CF_FOR_K8s_DIR="${CF_FOR_K8s_DIR:-${HOME}/workspace/cf-for-k8s/}"
+SCRIPT_DIR="$(dirname $(realpath $0))"
+BASE_DIR="${SCRIPT_DIR}/.."
+
+pushd "${CF_FOR_K8s_DIR}"
+  vendir sync -d config/_ytt_lib/github.com/cloudfoundry/capi-k8s-release="${BASE_DIR}"
+popd
