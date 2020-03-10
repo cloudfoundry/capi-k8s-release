22442aaca58cbee8b905bccf5c46a71b843fc5b4
Chris Selzo <cselzo@pivotal.io>
2020-03-10 15:56:44 -0700

    Remove cloud_controller_ng Dockerfile
    - This lives in the cloud_controller_ng repository now
    - Also remove unused `lifecycle_hooks` directory
    
    [#171387580]
    
    Co-authored-by: Chris Selzo <cselzo@pivotal.io>
    Co-authored-by: Sannidhi Jalukar <sjalukar@pivotal.io>

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR="$(dirname $0)"
 PARENT_DIR="${SCRIPT_DIR}/.."
 
 pushd ${PARENT_DIR}
-  docker build src/ -f dockerfiles/cloud_controller_ng/Dockerfile "$@"
+  docker build src/cloud_controller_ng -f src/cloud_controller_ng/Dockerfile "$@"
 popd
