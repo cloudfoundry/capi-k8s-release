9ebe96f9e75db1925aa30a4c1b4532b006e87298
Connor Braa <cbraa@pivotal.io>
2020-03-20 14:02:25 -0700

    remove unused scripts from pre-cf-for-k8s beforetimes
    Authored-by: Connor Braa <cbraa@pivotal.io>

--- scripts/minikube-rollout.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR=$(dirname $0)
-REPO_BASE_DIR="${SCRIPT_DIR}/.."
-
-eval $(minikube docker-env)
-${SCRIPT_DIR}/rollout.sh "$@" \
-	-f dev-templates/ \
-	-v kbld.destination="$(minikube ip)":5000/cloudfoundry/cloud-controller-ng
