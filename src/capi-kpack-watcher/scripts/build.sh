97f0fbebd5d888084360e05b1735b14070534c20
Aakash Shah <aashah@pivotal.io>
2020-04-06 15:55:04 -0700

    Update build script to account for new Dockerfile location
    [#171886837]
    
    Co-authored-by: Aakash Shah <aashah@pivotal.io>
    Co-authored-by: Renee Chu <rchu@pivotal.io>

--- src/capi-kpack-watcher/scripts/build.sh
+++ src/capi-kpack-watcher/scripts/build.sh
@@ -5,4 +5,4 @@ set -ex
 SCRIPT_DIR="$(dirname $0)"
 REPO_BASE_DIR="${SCRIPT_DIR}/../../../"
 
-docker build -f "${REPO_BASE_DIR}/dockerfiles/capi-kpack-watcher/Dockerfile" -t cloudfoundry/capi-kpack-watcher "${REPO_BASE_DIR}/src/"
+docker build -t cloudfoundry/capi-kpack-watcher "${REPO_BASE_DIR}/src/capi-kpack-watcher"
