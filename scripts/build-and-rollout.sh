9f252044d8b2d09518476c18b7a45696ab5901fd
Connor Braa <cbraa@pivotal.io>
2020-05-15 15:46:37 -0700

    fix build-and-rollout script
    - changed approach - instead of building into our default values
      we kbld against the fully built cf-for-k8s ymls
    - this obviates the need for build-into-values, so i removed that
    
    [#172482551]
    
    Authored-by: Connor Braa <cbraa@pivotal.io>

--- scripts/build-and-rollout.sh
+++ scripts/build-and-rollout.sh
@@ -4,10 +4,20 @@ set -ex
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REPO_BASE_DIR="${SCRIPT_DIR}/.."
+
+# set defaults - any of these should be configrable from outside this script
+IMAGE_DESTINATION="${IMAGE_DESTINATION:-gcr.io/cf-capi-arya/shared-dev-capi}"
+CF_FOR_K8S_DIR="${CF_FOR_K8s_DIR:-${HOME}/workspace/cf-for-k8s}"
+CCNG_DIR="${CCNG_DIR:-${REPO_BASE_DIR}/../capi-release/src/cloud_controller_ng}"
 : "${@?No values file supplied.}"
 
-${SCRIPT_DIR}/build-into-values.sh "${REPO_BASE_DIR}/values/images.yml" "${IMAGE_DESTINATION}"
+KBLD_TMP="$(mktemp)"
+ytt -f "${REPO_BASE_DIR}/dev-templates/" \
+    -v kbld.destination="${IMAGE_DESTINATION}" \
+    -v ccng_dir="${CCNG_DIR}" \
+     > "${KBLD_TMP}"
 
-${SCRIPT_DIR}/rollout.sh "$@"
+${SCRIPT_DIR}/bump-cf-for-k8s.sh
 
-
+kapp deploy -y -a cf \
+  -f <(kbld -f "${KBLD_TMP}" -f <(ytt -f "${CF_FOR_K8S_DIR}/config" -f "$@" ))
