571967d739907be988258b2386ad5299b6d95aa8
Connor Braa <cbraa@pivotal.io>
2020-05-15 15:55:39 -0700

    add some comments to build-and-rollout to make it grokkable
    [#172482551]
    
    Authored-by: Connor Braa <cbraa@pivotal.io>

--- scripts/build-and-rollout.sh
+++ scripts/build-and-rollout.sh
@@ -11,6 +11,7 @@ CF_FOR_K8S_DIR="${CF_FOR_K8s_DIR:-${HOME}/workspace/cf-for-k8s}"
 CCNG_DIR="${CCNG_DIR:-${REPO_BASE_DIR}/../capi-release/src/cloud_controller_ng}"
 : "${@?No values file supplied.}"
 
+# build parameters into the kbld configuration
 KBLD_TMP="$(mktemp)"
 ytt -f "${REPO_BASE_DIR}/dev-templates/" \
     -v kbld.destination="${IMAGE_DESTINATION}" \
@@ -19,5 +20,7 @@ ytt -f "${REPO_BASE_DIR}/dev-templates/" \
 
 ${SCRIPT_DIR}/bump-cf-for-k8s.sh
 
+# use kbld to process templated cf-for-k8s yml -
+# it'll find cloudfoundry/cloud_controller_ng and replace it w/ kbld-built image
 kapp deploy -y -a cf \
   -f <(kbld -f "${KBLD_TMP}" -f <(ytt -f "${CF_FOR_K8S_DIR}/config" -f "$@" ))
