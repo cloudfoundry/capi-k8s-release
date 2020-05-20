49e96a0f0ce1564c17cf0f2c7dca5297ce1cd580
Piyali Banerjee <pbanerjee@pivotal.io>
2020-05-20 16:31:22 -0700

    Add correct kbld values to build-and-rollout.
    Co-authored-by: Piyali Banerjee <pbanerjee@pivotal.io>
    Co-authored-by: Connor Braa <cbraa@pivotal.io>

--- scripts/build-and-rollout.sh
+++ scripts/build-and-rollout.sh
@@ -9,13 +9,16 @@ REPO_BASE_DIR="${SCRIPT_DIR}/.."
 IMAGE_DESTINATION="${IMAGE_DESTINATION:-gcr.io/cf-capi-arya/shared-dev-capi}"
 CF_FOR_K8S_DIR="${CF_FOR_K8s_DIR:-${HOME}/workspace/cf-for-k8s}"
 CCNG_DIR="${CCNG_DIR:-${REPO_BASE_DIR}/../capi-release/src/cloud_controller_ng}"
+CAPI_KPACK_WATCHER_DIR="${CAPI_KPACK_WATCHER_DIR:-${REPO_BASE_DIR}/src/capi-kpack-watcher}"
 : "${@?No values file supplied.}"
 
 # build parameters into the kbld configuration
 KBLD_TMP="$(mktemp)"
 ytt -f "${REPO_BASE_DIR}/dev-templates/" \
-    -v kbld.destination="${IMAGE_DESTINATION}" \
-    -v ccng_dir="${CCNG_DIR}" \
+    -v kbld.destination.ccng="${IMAGE_DESTINATION}" \
+    -v kbld.destination.capi_kpack_watcher="${IMAGE_DESTINATION}" \
+    -v src_dirs.ccng="${CCNG_DIR}" \
+    -v src_dirs.capi_kpack_watcher="${CAPI_KPACK_WATCHER_DIR}" \
      > "${KBLD_TMP}"
 
 ${SCRIPT_DIR}/bump-cf-for-k8s.sh
