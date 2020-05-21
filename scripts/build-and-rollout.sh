d1b5b665420f4dd0226f1db42516386170fb792a
Connor Braa <cbraa@pivotal.io>
2020-05-21 15:20:37 -0700

    Revert "fix build-and-rollout script" for faster config deploys
    - with the fix we put in last friday, it wasn't possible to `rollout.sh`
      using `build-and-rollout`'d images, as they'd get overridden.
    - by using the old approach of building digests into images.yml &
      vendir'ing those into cf-for-k8s, we regain that ability.
    
    This reverts commit 9f252044d8b2d09518476c18b7a45696ab5901fd.

--- scripts/build-and-rollout.sh
+++ scripts/build-and-rollout.sh
@@ -5,25 +5,7 @@ set -ex
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REPO_BASE_DIR="${SCRIPT_DIR}/.."
 
-# set defaults - any of these should be configrable from outside this script
-IMAGE_DESTINATION="${IMAGE_DESTINATION:-gcr.io/cf-capi-arya/shared-dev-capi}"
-CF_FOR_K8S_DIR="${CF_FOR_K8s_DIR:-${HOME}/workspace/cf-for-k8s}"
-CCNG_DIR="${CCNG_DIR:-${REPO_BASE_DIR}/../capi-release/src/cloud_controller_ng}"
-CAPI_KPACK_WATCHER_DIR="${CAPI_KPACK_WATCHER_DIR:-${REPO_BASE_DIR}/src/capi-kpack-watcher}"
-: "${@?No values file supplied.}"
+# additional environment variable overrides are available in this other script
+${SCRIPT_DIR}/build-into-values.sh "${REPO_BASE_DIR}/values/images.yml"
 
-# build parameters into the kbld configuration
-KBLD_TMP="$(mktemp)"
-ytt -f "${REPO_BASE_DIR}/dev-templates/" \
-    -v kbld.destination.ccng="${IMAGE_DESTINATION}" \
-    -v kbld.destination.capi_kpack_watcher="${IMAGE_DESTINATION}" \
-    -v src_dirs.ccng="${CCNG_DIR}" \
-    -v src_dirs.capi_kpack_watcher="${CAPI_KPACK_WATCHER_DIR}" \
-     > "${KBLD_TMP}"
-
-${SCRIPT_DIR}/bump-cf-for-k8s.sh
-
-# use kbld to process templated cf-for-k8s yml -
-# it'll find cloudfoundry/cloud_controller_ng and replace it w/ kbld-built image
-kapp deploy -y -a cf \
-  -f <(kbld -f "${KBLD_TMP}" -f <(ytt -f "${CF_FOR_K8S_DIR}/config" -f "$@" ))
+${SCRIPT_DIR}/rollout.sh "$@"
---
+++ scripts/build-into-values.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -ex
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REPO_BASE_DIR="${SCRIPT_DIR}/.."
+
+# set defaults - any of these should be configrable from outside this script
+IMAGE_DESTINATION="${IMAGE_DESTINATION:-gcr.io/cf-capi-arya/shared-dev-capi}"
+CF_FOR_K8s_DIR="${CF_FOR_K8s_DIR:-${HOME}/workspace/cf-for-k8s/}"
+CAPI_RELEASE_DIR="${CAPI_RELEASE_DIR:-${HOME}/workspace/capi-release/}"
+CCNG_DIR="${CAPI_RELEASE_DIR}src/cloud_controller_ng"
+CAPI_KPACK_WATCHER_DIR="${CAPI_KPACK_WATCHER_DIR:-${REPO_BASE_DIR}/src/capi-kpack-watcher}"
+
+if [ -z "$1" ]
+  then
+    echo "No values file was supplied!"
+    exit 1
+fi
+
+# template the image destination into the kbld yml
+KBLD_TMP="$(mktemp)"
+ytt -f "${REPO_BASE_DIR}/dev-templates/" \
+    -v kbld.destination.ccng="${IMAGE_DESTINATION}" \
+    -v kbld.destination.capi_kpack_watcher="${IMAGE_DESTINATION}" \
+    -v src_dirs.ccng="${CCNG_DIR}" \
+    -v src_dirs.capi_kpack_watcher="${CAPI_KPACK_WATCHER_DIR}" \
+     > "${KBLD_TMP}"
+
+# build a new values file with kbld
+pushd "${REPO_BASE_DIR}"
+  VALUES_TMP="$(mktemp)"
+  echo "#@data/values" > "${VALUES_TMP}"
+  kbld --images-annotation=false -f "${KBLD_TMP}" -f "$1" >> "${VALUES_TMP}"
+popd
+
+cat "${VALUES_TMP}" > "$1"
