a96a91861667f9e2b93b40fac2f8eab6e0a61de9
Piyali Banerjee <pbanerjee@pivotal.io>
2020-05-26 15:59:35 -0700

    Fixed image destinations for ccng & kpack watcher
    - Image destinations for ccng and kpack watcher now point to different
    ImageRepos. IMAGE_DESTINATION_CCNG and IMAGE_DESTINATION_KPACK_WATCHER
    can be configurable and if not set as an env var, they default to
    gcr.io/cf-capi-arya/dev-ccng and gcr.io/cf-capi-arya/dev-kpack-watcher
    respectively.
    
    Co-authored-by: Piyali Banerjee <pbanerjee@pivotal.io>
    Co-authored-by: Sannidhi Jalukar <sjalukar@pivotal.io>

--- scripts/build-into-values.sh
+++ scripts/build-into-values.sh
@@ -6,7 +6,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REPO_BASE_DIR="${SCRIPT_DIR}/.."
 
 # set defaults - any of these should be configrable from outside this script
-IMAGE_DESTINATION="${IMAGE_DESTINATION:-gcr.io/cf-capi-arya/shared-dev-capi}"
+IMAGE_DESTINATION_CCNG="${IMAGE_DESTINATION_CCNG:-gcr.io/cf-capi-arya/dev-ccng}"
+IMAGE_DESTINATION_KPACK_WATCHER="${IMAGE_DESTINATION_KPACK_WATCHER:-gcr.io/cf-capi-arya/dev-kpack-watcher}"
 CF_FOR_K8s_DIR="${CF_FOR_K8s_DIR:-${HOME}/workspace/cf-for-k8s/}"
 CAPI_RELEASE_DIR="${CAPI_RELEASE_DIR:-${HOME}/workspace/capi-release/}"
 CCNG_DIR="${CAPI_RELEASE_DIR}src/cloud_controller_ng"
@@ -21,8 +22,8 @@ fi
 # template the image destination into the kbld yml
 KBLD_TMP="$(mktemp)"
 ytt -f "${REPO_BASE_DIR}/dev-templates/" \
-    -v kbld.destination.ccng="${IMAGE_DESTINATION}" \
-    -v kbld.destination.capi_kpack_watcher="${IMAGE_DESTINATION}" \
+    -v kbld.destination.ccng="${IMAGE_DESTINATION_CCNG}" \
+    -v kbld.destination.capi_kpack_watcher="${IMAGE_DESTINATION_KPACK_WATCHER}" \
     -v src_dirs.ccng="${CCNG_DIR}" \
     -v src_dirs.capi_kpack_watcher="${CAPI_KPACK_WATCHER_DIR}" \
      > "${KBLD_TMP}"
