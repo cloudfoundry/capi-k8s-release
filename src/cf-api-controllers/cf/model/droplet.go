4d52e7308acc2c41221d2880bcdc405becc39c06
Piyali Banerjee <pbanerjee@pivotal.io>
2020-08-07 10:43:11 -0700

    ImageController patches droplet w rebased im ref
    [#172542372]
    Authored-by: Piyali Banerjee <pbanerjee@pivotal.io>

---
+++ src/cf-api-controllers/cf/model/droplet.go
@@ -0,0 +1,8 @@
+package model
+
+// Droplet represents the payload that will be sent to CF API server when an Image
+// has been rebased.
+type Droplet struct {
+	Image     string    `json:"image"`
+}
+
