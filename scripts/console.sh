e7a3d40d09f00cef8a1fa79f82a9037d73cf0a9d
Connor Braa <cbraa@pivotal.io>
2020-08-14 12:41:50 -0700

    scripts/console.sh update so it works w/ new secrets locations
    Authored-by: Connor Braa <cbraa@pivotal.io>

--- scripts/console.sh
+++ scripts/console.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
- kubectl -n cf-system exec -it deployments/cf-api-server -- /cloud_controller_ng/bin/console /config/cloud_controller_ng.yml
+ kubectl exec \
+   -n cf-system \
+   -it deployments/cf-api-clock \
+   -- /cloud_controller_ng/bin/console
