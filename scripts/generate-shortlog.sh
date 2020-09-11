8e45b675c060e0d9fb8a0b06dacb3e17c1ade468
Connor Braa <cbraa@pivotal.io>
2020-09-11 13:11:19 -0700

    improved shortlog script
    - only notes when there are changes from the last recorded image
    - more machine parsable- ymlish
    - also seed the current state into the log below v
    
    ---
    ccng:
      image: cloudfoundry/cloud-controller-ng@sha256:7b2856677f079539f9fb7c1179df0e763077d9e6b18dca8394400c3e1bc879bf
      sha: c743feaae5fc3081a76c9a7ba8bd550f06ae2686
      remote: https://github.com/cloudfoundry/cloud_controller_ng
    nginx:
      image: cloudfoundry/capi-nginx@sha256:980f50e190cbff72d23300bc422da23faa888271c2d07ac3abaa65af55a5316a
      sha: 56c57636971a434658a0dc4e1225648b3bb26d85
      remote: https://github.com/cloudfoundry/capi-k8s-release.git
    cf_api_controllers:
      image: cloudfoundry/cf-api-controllers@sha256:9cd6557f411c42b2bafe8487f63fae608f0dafd9f56c4268de8ec6fcd88ff7d9
      sha: 56c57636971a434658a0dc4e1225648b3bb26d85
      remote: https://github.com/cloudfoundry/capi-k8s-release.git
    package_image_uploader:
      image: cloudfoundry/cf-api-package-image-uploader@sha256:aae727a0960d10ce644035dee7041f7e882c7c58a37992252002ce7c95d8804d
      sha: 56c57636971a434658a0dc4e1225648b3bb26d85
      remote: https://github.com/cloudfoundry/capi-k8s-release.git
    
    Authored-by: Connor Braa <cbraa@pivotal.io>

---
+++ scripts/generate-shortlog.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+set -e
+
+SHORT_MESSAGE="images.yml updated by CI"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REPO_BASE_DIR="${SCRIPT_DIR}/.."
+
+CAPI_RELEASE_DIR="${CAPI_RELEASE_DIR:-${HOME}/workspace/capi-release/}"
+CCNG_DIR="${CCNG_DIR:-${CAPI_RELEASE_DIR}src/cloud_controller_ng}"
+CF_API_CONTROLLERS_DIR="${CAPI_CF_API_CONTROLLERS_DIR:-${REPO_BASE_DIR}/src/cf-api-controllers}"
+PACKAGE_IMAGE_UPLOADER_DIR="${PACKAGE_IMAGE_UPLOADER_DIR:-${REPO_BASE_DIR}/src/package-image-uploader}"
+NGINX_DIR="${NGINX_DIR:-${REPO_BASE_DIR}/dockerfiles/nginx}"
+
+function git_sha() {
+  local repo=${1}
+  pushd "${repo}" > /dev/null
+    git rev-parse HEAD
+  popd >/dev/null
+}
+
+function git_remote() {
+  local repo=${1}
+  pushd "${repo}" > /dev/null
+    git remote get-url origin
+  popd >/dev/null
+}
+
+function get_image() {
+  local name=${1}
+  pushd "${REPO_BASE_DIR}" >/dev/null
+      yq -r ".images.${name}" values/images.yml
+  popd >/dev/null
+}
+
+function image_changed() {
+  local name=${1}
+  ! git log -n 1 --grep="^${name}:$" | grep "$(get_image "${name}")" > /dev/null
+}
+
+function chunk() {
+  local name=${1}
+  local dir=${2}
+  if image_changed "${name}"; then
+    cat <<- EOF
+${name}:
+  image: $(get_image "${name}")
+  sha: $(git_sha "${dir}")
+  remote: $(git_remote "${dir}")
+EOF
+  fi
+}
+
+cat <<- EOF
+${SHORT_MESSAGE}
+---
+EOF
+chunk ccng "${CCNG_DIR}"
+chunk nginx "${NGINX_DIR}"
+chunk cf_api_controllers "${CF_API_CONTROLLERS_DIR}"
+chunk package_image_uploader "${PACKAGE_IMAGE_UPLOADER_DIR}"
