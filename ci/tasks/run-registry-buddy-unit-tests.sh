66bc3c34d67f559f1451ceb7ed6e3f4bcf8b2f5d
Caitlyn Yu <caitlyny@vmware.com>
2021-05-17 15:15:26 -0600

    Fix up references to CI directory in task scripts
    [#178057978](https://www.pivotaltracker.com/story/show/178057978)

--- ci/tasks/run-registry-buddy-unit-tests.sh
+++ ci/tasks/run-registry-buddy-unit-tests.sh
@@ -2,9 +2,13 @@
 
 set -eu
 
+# INPUTS
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+workspace_dir="$( cd "${script_dir}/../../../" && pwd )"
+
 go get github.com/onsi/ginkgo/ginkgo
 go install github.com/onsi/ginkgo/ginkgo
 
-pushd "registry-buddy/src/registry-buddy" >/dev/null
+pushd "${workspace_dir}/registry-buddy/src/registry-buddy" >/dev/null
      ginkgo -r -keepGoing -p -trace -randomizeAllSpecs -progress --race
 popd >/dev/null
